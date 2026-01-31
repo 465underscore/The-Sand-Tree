8b5a76efb6efa9490d50e21de8146a57faf6ea30
465_ <145865616+465underscore@users.noreply.github.com>
2026-01-30 21:51:37 -0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,78 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+	upgrades: {
+		11: {
+			name: "Speed Up",
+			description: "Point generation *1.2.",
+			cost: new Decimal(1),
+			effect() {
+				let eff = new Decimal(1.2)
+				if (hasUpgrade('p', 22)) {eff.times(upgradeEffect('p', 22))}
+				return eff
+			}
+		},
+		12: {
+			name: "Prestige Power",
+			description: "Prestige points boost point generation.",
+			cost: new Decimal(1),
+			effect() {
+				let eff = player.p.points.plus(2).pow(0.33)
+				if (hasUpgrade('p', 22)) {eff.times(upgradeEffect('p', 22))}
+				return eff
+			}
+		},
+		13: {
+			name: "Sufficiency",
+			description: "Points boost their own generation.",
+			cost: new Decimal(5),
+			effect() {
+				let eff = player.points.times(6).log(10)
+				if (hasUpgrade('p', 22)) {eff.times(upgradeEffect('p', 22))}
+				return eff
+			}
+		},
+		21: {
+			name: "Commonality",
+			description: "Prestige point gain *1.8.",
+			cost: new Decimal(20),
+			effect() {
+				let eff = new Decimal(1.8)
+				if (hasUpgrade('p', 22)) {eff.times(upgradeEffect('p', 22))}
+				return eff
+			}
+		},
+		22: {
+			name: "Elegant Efficiency",
+			description: "All prestige upgrades *1.01.",
+			cost: new Decimal(75),
+			effect() {
+				let eff = new Decimal(1.01)
+				if (hasUpgrade('p', 32)) {eff.times(upgradeEffect('p', 32))}
+				return eff
+			}
+		},
+		23: {
+			name: "Point Power",
+			description: "Points boost prestige point generation.",
+			cost: new Decimal(500),
+			effect() {return player.points.times(7).log(9)}
+		},
+		31: {
+			name: "Abundance",
+			description: "Prestige point gain ^1.05.",
+			cost: new Decimal(1e45)
+		},
+		32: {
+			name: "Heuristics",
+			description: "Points boost Elegant Efficiency.",
+			cost: new Decimal(1e56)
+		},
+		33: {
+			name: "Vertical Tactics",
+			description: "Total prestige points boost Sufficiency and Point Power.",
+			cost: new Decimal(1e60)
+		}
+	}
 })
